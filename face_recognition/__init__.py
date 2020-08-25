46e6f70b7ad87ed966e9b389feaabfdc082e5046
Nageshkashyapp <64788915+Nageshkashyapp@users.noreply.github.com>
2020-08-25 16:17:39 +0530

    Update __init__.py

--- face_recognition/__init__.py
+++ face_recognition/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-__author__ = """Adam Geitgey"""
-__email__ = 'ageitgey@gmail.com'
+__author__ = """NAGESH KASHYAP P"""
+__email__ = 'kashyapnagesh2000@gmail.com'
 __version__ = '1.2.3'
 
 from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
